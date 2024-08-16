0fcb66ca4cac71a5209dccbc8209b917ee4e2929
Feliks Aliaj <feliks.aliaj@gmail.com>
2024-08-16 16:32:00 +0200

    hotfix

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -2,7 +2,9 @@
 import { useState, useCallback, useLayoutEffect } from 'react'
 
 export const useIsMobile = (maxWidth: number = 700) => {
-	const [isMobile, setIsMobile] = useState(window.innerWidth <= maxWidth)
+	const [isMobile, setIsMobile] = useState(() => {
+		if (typeof window !== 'undefined') return window.innerWidth <= maxWidth
+	})
 
 	const handleResize = useCallback(() => {
 		setIsMobile(window.innerWidth <= maxWidth)
